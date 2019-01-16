6648407eac135efe83e014408881d2d448a5d8c0
Andrew <sit-surgut@mail.ru>
2019-01-16 09:48:18 +0500

    add class

---
+++ app/src/main/java/ithm/ru/MainView.java
@@ -0,0 +1,9 @@
+package ithm.ru;
+
+public interface MainView {
+    void setSeconds(int value);
+
+    void setMinute(int value);
+
+    void setHours(int value);
+}
---
+++ app/src/main/java/ithm/ru/MvpMainActivity.java
@@ -0,0 +1,62 @@
+package ithm.ru;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MvpMainActivity extends AppCompatActivity implements MainView, View.OnClickListener {
+
+    private Button btnCounter1;
+    private Button btnCounter2;
+    private Button btnCounter3;
+    private Presenter mPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnCounter1 = findViewById(R.id.btnCounter1);
+        btnCounter2 = findViewById(R.id.btnCounter2);
+        btnCounter3 = findViewById(R.id.btnCounter3);
+        btnCounter1.setOnClickListener(this);
+        btnCounter2.setOnClickListener(this);
+        btnCounter3.setOnClickListener(this);
+        mPresenter = new Presenter(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnCounter1: {
+                mPresenter.incSec();
+                break;
+            }
+            case R.id.btnCounter2: {
+                mPresenter.incMin();
+                break;
+            }
+            case R.id.btnCounter3: {
+                mPresenter.incHours();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void setSeconds(int value) {
+        btnCounter1.setText("Количество = " + value);
+    }
+
+    @Override
+    public void setMinute(int value) {
+        btnCounter2.setText("Количество = " + value);
+    }
+
+    @Override
+    public void setHours(int value) {
+        btnCounter3.setText("Количество = " + value);
+    }
+}
+
