536db4621c35fa157ec6283b114d1516d14e12a0
Andrew <sit-surgut@mail.ru>
2019-01-27 11:26:23 +0500

    Init commit HW3

---
+++ app/src/main/java/ithm/ru/hw3/LFMainActivity.java
@@ -0,0 +1,15 @@
+package ithm.ru.hw3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ithm.ru.R;
+
+public class LFMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lfmain);
+    }
+}
